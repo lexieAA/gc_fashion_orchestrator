0b6525ea0ce7f6cd27f61ddf0340576387621777
jalvesteffer <jason.alvesteffer@smoothstack.com>
2020-06-17 00:35:55 -0400

    Added transaction methods to orchestrator api

--- src/main/java/com/smoothstack/gcfashion/orchestrator/controller/SecurityControlledAPIs.java
+++ src/main/java/com/smoothstack/gcfashion/orchestrator/controller/SecurityControlledAPIs.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,13 +15,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.smoothstack.gcfashion.orchestrator.entity.Transaction;
 //import com.smoothstack.gcfashion.orchestrator.db.UserDAO;
 import com.smoothstack.gcfashion.orchestrator.entity.User;
 
 @RestController
 @RequestMapping("/gcfashions")
 public class SecurityControlledAPIs {
-	//private UserDAO userDao;
+	// private UserDAO userDao;
 
 	@Autowired
 	RestTemplate restTemplate;
@@ -30,12 +32,14 @@ public class SecurityControlledAPIs {
 //	}
 
 	public static final int ONLINE_SERVICE = 8081;
+	public static final int SALES_SERVICE = 8082;
 
 	// ------------Requests for Online Stores-----------------------
 	// read all products
 	@GetMapping(path = "/shop/products")
 	public ResponseEntity<String> allProducts() throws SQLException {
-		return restTemplate.getForEntity("http://localhost:" + ONLINE_SERVICE + "/gcfashions/shop/products", String.class);
+		return restTemplate.getForEntity("http://localhost:" + ONLINE_SERVICE + "/gcfashions/shop/products",
+				String.class);
 	}
 
 //	// read by branch Id
@@ -52,8 +56,37 @@ public class SecurityControlledAPIs {
 //				String.class);
 //	}
 
+	//
+	//
+	// ------------ Requests for Sales --------------------------------
+	//
+	//
 
-	/// ------------ Requests for Borrower --------------------------------
+	// read a transaction by id
+	@GetMapping(path = "/sales/transactions/{id}")
+	public ResponseEntity<String> getTransactionById(@PathVariable Long id) {
+		return restTemplate.getForEntity("http://localhost:" + SALES_SERVICE + "/gcfashions/sales/transactions/" + id,
+				String.class);
+	}
 
+	// update a transaction
+	@PutMapping(path = "/sales/transactions")
+	public void updateTransaction(@RequestBody Transaction t) {
+		restTemplate.put("http://localhost:" + SALES_SERVICE + "/gcfashions/sales/transactions", t, String.class);
+	}
+
+	// create a transaction
+	@PostMapping(path = "/sales/transactions")
+	public ResponseEntity<String> createTransaction(@RequestBody Transaction t) {
+		return restTemplate.postForEntity("http://localhost:" + SALES_SERVICE + "/gcfashions/sales/transactions", t,
+				String.class);
+	}
+
+	// read a transaction by id
+	@DeleteMapping(path = "/sales/transactions")
+	public void deleteTransaction(@PathVariable Long id) {
+		restTemplate.delete("http://localhost:" + SALES_SERVICE + "/gcfashions/sales/transactions/" + id,
+				String.class);
+	}
 
 }
