6951b64692abce69ed7ccce7a7a0e5b385183ec8
jalvesteffer <jason.alvesteffer@smoothstack.com>
2020-06-18 19:13:56 -0400

    Orchestra works with basic HTTP auth

--- src/main/java/com/smoothstack/gcfashion/orchestrator/security/SecurityConfiguration.java
+++ src/main/java/com/smoothstack/gcfashion/orchestrator/security/SecurityConfiguration.java
@@ -17,51 +17,62 @@ import com.smoothstack.gcfashion.orchestrator.db.UserDAO;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    private UserPrincipalDetailsService userPrincipalDetailsService;
-    private UserDAO userDao;
-
-    public SecurityConfiguration(UserPrincipalDetailsService userPrincipalDetailsService, UserDAO userDao) {
-        this.userPrincipalDetailsService = userPrincipalDetailsService;
-        this.userDao = userDao;
-    }
-
+//    private UserPrincipalDetailsService userPrincipalDetailsService;
+//    private UserDAO userDao;
+//
+//    public SecurityConfiguration(UserPrincipalDetailsService userPrincipalDetailsService, UserDAO userDao) {
+//        this.userPrincipalDetailsService = userPrincipalDetailsService;
+//        this.userDao = userDao;
+//    }
+//
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authenticationProvider());
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    	auth
+    		.inMemoryAuthentication()
+    		.withUser("admin").password(passwordEncoder().encode("admin123")).roles("ADMIN")
+    		.and()
+    		.withUser("dan").password(passwordEncoder().encode("dan123")).roles("USER");
+    	
+    	
+//        auth.authenticationProvider(authenticationProvider());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                // remove csrf and state in session because in jwt we do not need them
-                .csrf().disable()
-                //removed, we do not want a sticky session
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                // add jwt filters (1. authentication, 2. authorization)
-                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
-				.addFilter(new JwtAuthorizationFilter(authenticationManager(),
-						this.userDao))
-                .authorizeRequests()
-                // configure access rules
-                .antMatchers(HttpMethod.POST, "/login").permitAll()
-                .antMatchers(HttpMethod.GET, "/gcfashions/shop/products").permitAll()
-                .antMatchers(HttpMethod.PUT, "/gcfashions/new/account").permitAll()
-                .antMatchers("/gcfashions/shop/*").permitAll()
-                .antMatchers("/gcfashions/account/*").hasRole("CUSTOMER")
-                .antMatchers("/gcfashions/sales/*").hasRole("SALES")
-                .antMatchers("/gcfashions/account/*").hasRole("MANAGENT")
-                .anyRequest().authenticated();
+        	.authorizeRequests()
+        	.anyRequest().authenticated()
+        	.and()
+        	.httpBasic();
+//                // remove csrf and state in session because in jwt we do not need them
+//                .csrf().disable()
+//                //removed, we do not want a sticky session
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                // add jwt filters (1. authentication, 2. authorization)
+//                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+//				.addFilter(new JwtAuthorizationFilter(authenticationManager(),
+//						this.userDao))
+//                .authorizeRequests()
+//                // configure access rules
+//                .antMatchers(HttpMethod.POST, "/login").permitAll()
+//                .antMatchers(HttpMethod.GET, "/gcfashions/shop/products").permitAll()
+//                .antMatchers(HttpMethod.PUT, "/gcfashions/new/account").permitAll()
+//                .antMatchers("/gcfashions/shop/*").permitAll()
+//                .antMatchers("/gcfashions/account/*").hasRole("CUSTOMER")
+//                .antMatchers("/gcfashions/sales/*").hasRole("SALES")
+//                .antMatchers("/gcfashions/account/*").hasRole("MANAGENT")
+//                .anyRequest().authenticated();
     }
 
-    @Bean
-    DaoAuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-        daoAuthenticationProvider.setUserDetailsService(this.userPrincipalDetailsService);
-
-        return daoAuthenticationProvider;
-    }
+//    @Bean
+//    DaoAuthenticationProvider authenticationProvider(){
+//        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+//        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+//        daoAuthenticationProvider.setUserDetailsService(this.userPrincipalDetailsService);
+//
+//        return daoAuthenticationProvider;
+//    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
