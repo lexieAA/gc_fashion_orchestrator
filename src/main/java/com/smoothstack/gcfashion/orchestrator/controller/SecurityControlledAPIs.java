ccf8d4cf488cef53151a4769d0d73b1696d1d3f1
Lexie <Lexie@LAPTOP-J8UR8ODG>
2020-06-18 22:44:57 -0700

    updated controllor and added CrossOrigin

--- src/main/java/com/smoothstack/gcfashion/orchestrator/controller/SecurityControlledAPIs.java
+++ src/main/java/com/smoothstack/gcfashion/orchestrator/controller/SecurityControlledAPIs.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,15 +35,18 @@ public class SecurityControlledAPIs {
 
 	public static final int ONLINE_SERVICE = 8081;
 	public static final int SALES_SERVICE = 8082;
+	public static final int ACCOUNT_SERVICE = 8083;
 
 	// ------------Requests for Online Stores-----------------------
 		// read all products
+		@CrossOrigin(origins="http://localhost:8080")
 		@GetMapping(path = "/shop/products")
 		public ResponseEntity<String> allProducts() throws SQLException {
 			return restTemplate.getForEntity("http://localhost:" + ONLINE_SERVICE + "/gcfashions/shop/products", String.class);
 		}
 
 		// read by products by Id
+		@CrossOrigin(origins="http://localhost:8080")
 		@GetMapping("/shop/products/{productId}")
 		public ResponseEntity<String> allProductsByIdOnline(@PathVariable Long productId) throws SQLException {
 			return restTemplate.getForEntity("http://localhost:" + ONLINE_SERVICE + "/gcfashions/shop/products/" + productId,
@@ -49,12 +54,14 @@ public class SecurityControlledAPIs {
 		}
 		
 		// read all coupons
+		@CrossOrigin(origins="http://localhost:8080")
 		@GetMapping(path = "/shop/coupons")
 		public ResponseEntity<String> allCouponsOnline() throws SQLException {
 			return restTemplate.getForEntity("http://localhost:" + ONLINE_SERVICE + "/gcfashions/shop/coupons", String.class);
 		}
 		
 		// read by transaction by Id
+		@CrossOrigin(origins="http://localhost:8080")
 		@GetMapping("/account/users/{userId}/transactions")
 		public ResponseEntity<String> allTransactionsByIdOnline(@PathVariable Long userId) throws SQLException {
 			return restTemplate.getForEntity("http://localhost:" + ONLINE_SERVICE + "/gcfashions/shop/transactions/" + userId,
@@ -62,6 +69,7 @@ public class SecurityControlledAPIs {
 		}
 
 		 //update a transaction
+		@CrossOrigin(origins="http://localhost:8080")
 		@PostMapping("/account/users/{userId}/transactions")
 		public void updateTransactionsByIdOnline(@RequestBody Transaction transaction, @PathVariable Long userId) throws SQLException {
 			restTemplate.postForEntity("http://localhost:" + ONLINE_SERVICE + "/gcfashions/shop/transactions/" + userId, transaction,
@@ -69,25 +77,22 @@ public class SecurityControlledAPIs {
 		}
 		
 		//create a transaction
+		@CrossOrigin(origins="http://localhost:8080")
 		@PutMapping("/account/users/{userId}/transactions")
 		public void createTransactionsByIdOnline(@RequestBody Transaction transaction) throws SQLException {
 			restTemplate.put("http://localhost:" + ONLINE_SERVICE + "/gcfashions/shop/transactions", transaction,
 					String.class);
 		}
 		
-//		@DeleteMapping("users/{userId}/transactions")
-//		public void delTransactionsByIdOnline(@RequestBody Transaction transaction, @PathVariable Long userId) throws SQLException {
-//			restTemplate.delete("http://localhost:" + ONLINE_SERVICE + "/gcfashions/shop/transactions/" + userId, transaction,
-//					String.class);
-//		}
-		
 		// read all categories
+		@CrossOrigin(origins="http://localhost:8080")
 		@GetMapping(path = "/shop/categories")
 		public ResponseEntity<String> allCategoriesOnline() throws SQLException {
 			return restTemplate.getForEntity("http://localhost:" + ONLINE_SERVICE + "/gcfashions/shop/categories", String.class);
 		}
 		
 		// read by products by category Id
+		@CrossOrigin(origins="http://localhost:8080")
 		@GetMapping("/shop/categories/{categoryId}/products")
 		public ResponseEntity<String> CategoriesByIdOnline(@PathVariable Long categoryId) throws SQLException {
 			return restTemplate.getForEntity("http://localhost:" + ONLINE_SERVICE + "/gcfashions/shop/categories/" + categoryId +"/products",
@@ -95,33 +100,37 @@ public class SecurityControlledAPIs {
 		}
 		
 		// read all subcategories
+		@CrossOrigin(origins="http://localhost:8080")
 		@GetMapping(path = "/shop/subcategories")
 		public ResponseEntity<String> allSubcategoriesOnline() throws SQLException {
 			return restTemplate.getForEntity("http://localhost:" + ONLINE_SERVICE + "/gcfashions/shop/subcategories", String.class);
 		}
 		
 		// read all stores
+		@CrossOrigin(origins="http://localhost:8080")
 		@GetMapping(path = "/shop/stores")
 		public ResponseEntity<String> allStoresOnline() throws SQLException {
 			return restTemplate.getForEntity("http://localhost:" + ONLINE_SERVICE + "/gcfashions/shop/stores", String.class);
 		}
 		
 		// read by users by user Id
+		@CrossOrigin(origins="http://localhost:8080")
 		@GetMapping("/shop/account/users/{userId}")
 		public ResponseEntity<String> userByIdOnline(@PathVariable Long userId) throws SQLException {
 			return restTemplate.getForEntity("http://localhost:" + ONLINE_SERVICE + "/gcfashions/shop/account/users/" + userId,
 					String.class);
 		}
-		
+
+		@CrossOrigin(origins="http://localhost:8080")
 		@PostMapping("/shop/account/users/{userId}")
 		public void updateUserByIdOnline(@RequestBody User user, @PathVariable Long userId) throws SQLException {
 			restTemplate.postForEntity("http://localhost:" + ONLINE_SERVICE + "/gcfashions/shop/account/users/" + userId, user,
 					String.class);
 		}
-		
+
+		@CrossOrigin(origins="http://localhost:8080")
 		@PutMapping("/new/account")
 		public void createUserByIdOnline(@RequestBody User user, @PathVariable Long userId) throws SQLException {
-			
 			restTemplate.put("http://localhost:" + ONLINE_SERVICE + "/gcfashions/shop/account/users", user,
 					String.class);
 		}
@@ -130,6 +139,7 @@ public class SecurityControlledAPIs {
 
 
 	// read a transaction by id
+	@CrossOrigin(origins="http://localhost:8080")
 	@GetMapping(path = "/sales/transactions/{id}")
 	public ResponseEntity<String> getTransactionById(@PathVariable Long id) {
 		return restTemplate.getForEntity("http://localhost:" + SALES_SERVICE + "/gcfashions/sales/transactions/" + id,
@@ -137,12 +147,14 @@ public class SecurityControlledAPIs {
 	}
 
 	// update a transaction
+	@CrossOrigin(origins="http://localhost:8080")
 	@PutMapping(path = "/sales/transactions")
 	public void updateTransaction(@RequestBody Transaction t) {
 		restTemplate.put("http://localhost:" + SALES_SERVICE + "/gcfashions/sales/transactions", t, String.class);
 	}
 
 	// create a transaction
+	@CrossOrigin(origins="http://localhost:8080")
 	@PostMapping(path = "/sales/transactions")
 	public ResponseEntity<String> createTransaction(@RequestBody Transaction t) {
 		return restTemplate.postForEntity("http://localhost:" + SALES_SERVICE + "/gcfashions/sales/transactions", t,
@@ -150,9 +162,33 @@ public class SecurityControlledAPIs {
 	}
 
 	// read a transaction by id
+	@CrossOrigin(origins="http://localhost:8080")
 	@DeleteMapping(path = "/sales/transactions/{id}")
 	public void deleteTransaction(@PathVariable Long id) {
 		restTemplate.delete("http://localhost:" + SALES_SERVICE + "/gcfashions/sales/transactions/" + id,
+				String.class);
+	}
+	
+	// ------------ Requests for Account --------------------------------
+	
+	@GetMapping(path = "/accountant")
+	@CrossOrigin(origins="http://localhost:8080")
+	public ResponseEntity<String> findAllManagers() {
+		return restTemplate.getForEntity("http://localhost:" + ACCOUNT_SERVICE + "/accountant",
+				String.class);
+	}
+	
+	@GetMapping(path = "/accountant/reports/sales")
+	@CrossOrigin(origins="http://localhost:8080")
+	public ResponseEntity<String> getSalesReport() {
+		return restTemplate.getForEntity("http://localhost:" + ACCOUNT_SERVICE + "/accountant/reports/sales",
+				String.class);
+	}
+	
+	@GetMapping(path = "/accountant/reports/taxes")
+	@CrossOrigin(origins="http://localhost:8080")
+	public ResponseEntity<String> getTaxReport() {
+		return restTemplate.getForEntity("http://localhost:" + ACCOUNT_SERVICE + "/accountant/reports/taxes",
 				String.class);
 	}
 
