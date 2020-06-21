fa65d58ec7126fd357fd3399026257d2db1f1840
lexieAA <av697@nau.edu>
2020-06-21 12:05:11 -0700

    update for shop configurations

--- src/main/java/com/smoothstack/gcfashion/orchestrator/security/SecurityConfiguration.java
+++ src/main/java/com/smoothstack/gcfashion/orchestrator/security/SecurityConfiguration.java
@@ -45,7 +45,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	        .authorizeRequests()
 	        // configure access rules
 	        .antMatchers(HttpMethod.POST, "/login").permitAll()
-	        .antMatchers(HttpMethod.GET, "/gcfashions/shop/products").permitAll()
+	        .antMatchers(HttpMethod.GET, "/gcfashions/shop/**").permitAll()
 	        .antMatchers("/gcfashions/account/**").hasAuthority("ROLE_CUSTOMER")
 	        .antMatchers("/gcfashions/sales/**").hasAnyRole("ADMIN", "MANAGER")
 	        .antMatchers("/gcfashions/accountant/**").hasAnyRole("MANAGER")
