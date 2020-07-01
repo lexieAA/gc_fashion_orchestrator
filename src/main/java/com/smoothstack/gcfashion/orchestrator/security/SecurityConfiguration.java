10e7ad7dafddd01d8bcf0d420e628ccba11ebbcb
lexieAA <av697@nau.edu>
2020-07-01 15:55:53 -0700

    saves userid header

--- src/main/java/com/smoothstack/gcfashion/orchestrator/security/SecurityConfiguration.java
+++ src/main/java/com/smoothstack/gcfashion/orchestrator/security/SecurityConfiguration.java
@@ -69,6 +69,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		config.addAllowedOrigin("*"); // TODO: lock down before deploying
 		config.addAllowedHeader("*");
 		config.addExposedHeader(HttpHeaders.AUTHORIZATION);
+		config.addExposedHeader("UserId");
 		config.addAllowedMethod("*");
 		source.registerCorsConfiguration("/**", config);
 		return new CorsFilter(source);
