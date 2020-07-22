0f8a7f785207c97da85a282ebccd16a8766e2952
jalvesteffer <63688073+jalvesteffer@users.noreply.github.com>
2020-07-21 23:06:35 -0400

    Update SecurityControlledAPIs.java

--- src/main/java/com/smoothstack/gcfashion/orchestrator/controller/SecurityControlledAPIs.java
+++ src/main/java/com/smoothstack/gcfashion/orchestrator/controller/SecurityControlledAPIs.java
@@ -36,10 +36,10 @@ public class SecurityControlledAPIs {
 		this.userDao = userDao;
 	}
 
-	public static final String ONLINE_SERVICE = "http://ec2-18-191-27-130.us-east-2.compute.amazonaws.com";
-	public static final String SALES_SERVICE = "http://ec2-18-219-254-63.us-east-2.compute.amazonaws.com";
-	public static final String ACCOUNT_SERVICE = "http://ec2-18-219-109-245.us-east-2.compute.amazonaws.com";
-	public static final String ANGULAR_APP = "http://gcfashion.s3-website-us-east-1.amazonaws.com";
+	public static final String ONLINE_SERVICE = "http://ec2-18-191-27-130.us-east-2.compute.amazonaws.com:8081";
+	public static final String SALES_SERVICE = "http://ec2-18-219-254-63.us-east-2.compute.amazonaws.com:8082";
+	public static final String ACCOUNT_SERVICE = "http://ec2-18-219-109-245.us-east-2.compute.amazonaws.com:8083";
+	public static final String ANGULAR_APP = "http://gcfashion.s3-website-us-east-1.amazonaws.com:8080";
 
 	// ------------Requests for Online Stores-----------------------
 	// read all products
